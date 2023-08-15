1d925466f84f8cdcc23421ebe19b30334d1777ba
camilasierraO <ospinasierracamila@gmail.com>
2023-08-15 17:20:11 -0500

    examen hecho

--- examen/examen1.dart
+++ examen/examen1.dart
@@ -10,25 +10,25 @@ void main() {
     op = int.parse(stdin.readLineSync()!);
 
     if (op == 1) {
-      convertirCelsiusAFahrenheit();
+      convertirCF();
     } else if (op == 2) {
-      convertirFahrenheitACelsius();
+      convertirFC();
     } else {
       print("Opción inválida.");
     }
   } while (op != 1 && op != 2);
 }
 
-void convertirFahrenheitACelsius() {
+void convertirFC() {
   print("Ingrese la temperatura en grados Fahrenheit:");
-  double fahrenheit = double.parse(stdin.readLineSync()!);
-  double celsius = (fahrenheit - 32) / 1.8;
-  print("$fahrenheit grados Fahrenheit son $celsius grados Celsius.");
+  double f = double.parse(stdin.readLineSync()!);
+  double c = (f - 32) / 1.8;
+  print("$f grados Fahrenheit son $c grados Celsius.");
 }
 
-void convertirCelsiusAFahrenheit() {
+void convertirCF() {
   print("Ingrese la temperatura en grados Celsius:");
-  double celsius = double.parse(stdin.readLineSync()!);
-  double fahrenheit = (celsius * 1.8) + 32;
-  print("$celsius grados Celsius son $fahrenheit grados Fahrenheit.");
+  double c = double.parse(stdin.readLineSync()!);
+  double f = (c * 1.8) + 32;
+  print("$c grados Celsius son $f grados Fahrenheit.");
 }
