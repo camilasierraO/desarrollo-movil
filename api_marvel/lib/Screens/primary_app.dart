4eddf6fd5edcac6bd26aed3817ebf6dab0cb8e07
camilasierraO <ospinasierracamila@gmail.com>
2023-09-23 14:03:34 -0500

    Creación de la primer pantalla

---
+++ api_marvel/lib/Screens/primary_app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Principal extends StatelessWidget {
+  const Principal({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          body: Center(child: Text("Hola mundoo")),
+        ));
+  }
+}
